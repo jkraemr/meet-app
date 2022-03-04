6a6985c65c72be3a008c7807a37f8f823e86b48c
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-03-04 17:24:12 +0100

    Add Atatus for Application Performance Monitoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('b772daa890a34180b869ee54fc7ba3db').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Test error event to check Atatus integration
+// atatus.notify(new Error('Test Atatus Setup'));
