7223e52b1516a0834cdce62a4abf649ae9d12534
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-03-09 16:04:42 +0100

    Add basic Scatter chart via Recharts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { NumberOfEvents } from './NumberOfEvents';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
+
 
 export class App extends Component {
 
@@ -86,6 +90,16 @@ export class App extends Component {
     }
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined &&
       navigator.onLine && !window.location.href.startsWith('http://localhost')
@@ -109,6 +123,21 @@ export class App extends Component {
           numberOfEvents={this.state.numberOfEvents}
           updateNumberOfEvents={this.updateNumberOfEvents}
           errorAlert={this.state.errorAlert} />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            // width={800}
+            // height={400}
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="City" />
+            <YAxis type="number" dataKey="number" name="Number of Events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => { getAccessToken() }} />
