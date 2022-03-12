ea9bd1b75614f5395ae9303c6ee0aab8f2f79560
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-03-13 00:35:35 +0100

    Update WelcomeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,40 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import './WelcomeScreen.css';
 
 function WelcomeScreen(props) {
+
+  const logo = require('./logo_transparent.webp');
+
   return props.showWelcomeScreen ?
     (
-      <div className="WelcomeScreen">
-        <h1>Welcome to the Meet app</h1>
-        <h4>
-          Log in to see upcoming events around the world for
-          full-stack
-          developers
-        </h4>
-        <div className="button_cont" align="center">
-          <div class="google-btn">
-            <div class="google-icon-wrapper">
-              <img
-                class="google-icon"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-o.svg"
-                alt="Google sign-in"
-              />
-            </div>
-            <button onClick={() => { props.getAccessToken() }}
-              rel="nofollow noopener"
-              class="btn-text"
-            >
-              <b>Sign in with google</b>
-            </button>
-          </div>
-        </div>
-        <a
-          href="https://jkraemr.github.io/meet-app/privacy.html"
-          rel="nofollow noopener"
-        >
-          Privacy policy
-        </a>
+      <div className='WelcomeScreen'>
+        <Container>
+          <img src='./logo_transparent.webp' alt='' />
+          <Row>
+            <Col className='welcome-card'>
+              <Card bg='dark' style={{ width: '25rem' }}>
+                <Card.Img className='welcome-card-img-top' variant='top' src={logo} />
+                <Card.Body className='welcome-card-body'>
+                  <Card.Title className='welcome-card-title'>meetFS</Card.Title>
+                  <Card.Text className='welcome-card-text'>
+                    Worldwide Events for Full-Stack Web Developers
+                  </Card.Text>
+                  <Button className='welcome-card-button' onClick={() => { props.getAccessToken() }} rel='nofollow noopener' variant='light'>Sign in with Google</Button>
+                  <Card.Text className='welcome-card-text-small'>
+                    <a
+                      href='https://jkraemr.github.io/meet-app/privacy.html'
+                      rel='nofollow noopener' target='_blank'
+                    >
+                      Privacy policy
+                    </a>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
     : null
